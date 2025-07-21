2acae2426a49b626067953126ca167564787d86e
Halil Yesilyurt <haliilyesilyurt@gmail.com>
2025-07-22 01:47:18 +0300

    feat: dynamic reading time in BlogPostLayout using calculateReadingTime utility

--- src/app/components/BlogPostLayout.tsx
+++ src/app/components/BlogPostLayout.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Sidebar from './Sidebar';
 import Breadcrumbs from './Breadcrumbs';
 import SocialShareButtons from './SocialShareButtons';
-import { getFeaturedImageUrl, WordPressPost, WordPressTag, WordPressCategory, decodeHtmlEntities } from '@/lib/wordpress';
+import { getFeaturedImageUrl, WordPressPost, WordPressTag, WordPressCategory, decodeHtmlEntities, calculateReadingTime } from '@/lib/wordpress';
 import Image from 'next/image';
 
 interface BlogPostLayoutProps {
@@ -103,7 +103,7 @@ export default function BlogPostLayout({ post, author, tags, posts, categories, 
                     <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    5 min read
+                    {calculateReadingTime(post.content.rendered)} min read
                   </div>
                 </div>
               </header>
